ea0a2447e4943d9749a9e1f1777b2ec143b29b93
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-07-09 19:59:02 -0400

    Update column name to 'label'

--- db/migrate/20190709223414_create_sports.rb
+++ db/migrate/20190709223414_create_sports.rb
@@ -1,7 +1,7 @@
 class CreateSports < ActiveRecord::Migration[5.2]
   def change
     create_table :sports do |t|
-      t.string :sport, null: false
+      t.string :label, null: false
       t.string :location
       t.string :date
 
