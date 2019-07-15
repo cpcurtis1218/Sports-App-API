a393555a7d2b56e8eeacec9b98537f1aab3cf983
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-07-15 14:10:19 -0400

    Updated schema with 'group' resource
    -Sport, date, and state are all required info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_223414) do
+ActiveRecord::Schema.define(version: 2019_07_15_175339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2019_07_09_223414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "sport", null: false
+    t.string "city"
+    t.string "state", null: false
+    t.string "date", null: false
+    t.string "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sports", force: :cascade do |t|
