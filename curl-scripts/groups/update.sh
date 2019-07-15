d1afdfa43557ddf71b9d7ee026a4ec559ce298e3
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-07-15 14:22:52 -0400

    Add curl script for updating a group (update)

---
+++ curl-scripts/groups/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl "http://localhost:4741/groups/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --data '{
+    "group": {
+      "sport": "'"${SPORT}"'"
+    }
+  }'
+
+echo
