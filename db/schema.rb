90d969b47703df268f70172adc155325fd8eefb3
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-16 10:41:13 -0400

    Updated schema with 'about' column in groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_175339) do
+ActiveRecord::Schema.define(version: 2019_08_16_141131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_175339) do
     t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "about"
   end
 
   create_table "sports", force: :cascade do |t|
