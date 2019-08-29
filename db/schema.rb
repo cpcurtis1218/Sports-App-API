f9f70755f8a2cd259814361f4f03375c40ccd6e1
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-29 18:13:18 -0400

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_141131) do
+ActiveRecord::Schema.define(version: 2019_08_29_212424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_08_16_141131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "about"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "sports", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2019_08_16_141131) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "groups", "users"
 end
