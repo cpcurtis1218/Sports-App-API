0c691f8a7056fb38b534234437926fcd26b41209
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-29 18:22:54 -0400

    Add auth header with token to create curl script

--- curl-scripts/groups/create.sh
+++ curl-scripts/groups/create.sh
@@ -4,6 +4,7 @@ curl "http://localhost:4741/groups" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "group": {
       "sport": "'"${SPORT}"'",
