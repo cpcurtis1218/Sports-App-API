3bfe310fa53c581c71399b435f36cc1dd866c22b
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-08-30 10:56:46 -0400

    Add Token to 'update' curl script

--- curl-scripts/groups/update.sh
+++ curl-scripts/groups/update.sh
@@ -4,6 +4,7 @@ curl "http://localhost:4741/groups/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "group": {
       "about": "'"${ABOUT}"'"
