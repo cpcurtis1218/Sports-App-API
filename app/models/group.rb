63d8c494e968809730b5a1d653d2ad90abf9b8af
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-04 19:06:41 -0400

    Add has_many relationship to user and group models
    -User has_many memberships
    -User has_many groups through memberships
    -Group has_many memberships
    -Group has_many users through memberships

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
   validates :user, presence: true
+  has_many :memberships
+  has_many :users, through: :memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_many :groups, dependent: :destroy
+  has_many :memberships
+  has_many :groups, through: :memberships
 end
