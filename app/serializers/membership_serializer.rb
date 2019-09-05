15fa0789047ae2f5aece2850b9d4d106a2f70935
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-05 14:40:59 -0400

    Add user_id and group_id attributes to membership serializer

--- app/serializers/membership_serializer.rb
+++ app/serializers/membership_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MembershipSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :group_id
   has_one :user
   has_one :group
 end
