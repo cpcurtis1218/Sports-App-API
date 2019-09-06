66cc2de140b131179f668d78ed5efdacfdb0641c
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-09-06 10:43:08 -0400

    Update group serializer
    -Remove users from serializer, only include memberships

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,4 +1,4 @@
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :sport, :city, :state, :date, :time, :about, :user_id
-  has_many :users, through: :memberships
+  has_many :memberships
 end
